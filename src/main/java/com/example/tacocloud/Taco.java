f41a56b2d96cc032084c26f1431b323de66431d6
Boopya <rikiyaeduarteyamamoto@gmail.com>
2023-08-02 21:52:14 +0800

    Add annotations for JPA persistence

--- src/main/java/com/example/tacocloud/Taco.java
+++ src/main/java/com/example/tacocloud/Taco.java
@@ -5,11 +5,20 @@ import java.util.List;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
+import jakarta.persistence.PrePersist;
 import lombok.Data;
 
 @Data
+@Entity
 public class Taco {
 
+    @Id
+    @GeneratedValue(strategy=GenerationType.AUTO)
     private Long id;
 
     private Date createdAt;
@@ -18,6 +27,12 @@ public class Taco {
     @Size(min=5, message="Name must be at least 5 characters long")
     private String name;
 
+    @ManyToMany(targetEntity=Ingredient.class)
     @Size(min=1, message="You must choose at least 1 ingredient")
     private List<Ingredient> ingredients;
+
+    @PrePersist
+    void createdAt() {
+        this.createdAt = new Date();
+    }
 }
